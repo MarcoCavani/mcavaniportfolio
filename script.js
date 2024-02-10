e50308918457142956d922e394d3617d762b1aca
marco cavani <cavaniaustralia@gmail.com>
2024-02-11 00:15:10 +0800

    file

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.getElementById('menu-toggle');
+    var menu = document.getElementById('menu');
+    
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('open');
+        menuToggle.classList.toggle('open');
+    });
+});
